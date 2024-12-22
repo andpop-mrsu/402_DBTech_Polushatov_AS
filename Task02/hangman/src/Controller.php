564c466cddea1d6634eb7b2c21432da990ce74ef
aplou00 <aplou00@mail.ru>
2024-12-23 00:10:26 +0300

    Task02

---
+++ Task02/hangman/src/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace aplou00\Hangman\Controller;
+
+use aplou00\Hangman\View;
+
+function startGame() {
+    View\showStartScreen();
+}
---
+++ Task02/hangman/src/View.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace aplou00\Hangman\View;
+
+function showStartScreen() {
+    \cli\line("Welcome to Hangman!");
+}
